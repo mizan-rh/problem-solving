947548ebe2fb5058b73ffdd648ad90f3344c1191
Mizanur Rahman <mizanrxb@gmail.com>
2025-05-21 23:54:46 +0600

    description 3 added

--- task-3.js
+++ task-3.js
@@ -1,3 +1,7 @@
+// Function Composition
+// Write three functions: one to square a number, one to double a number, and one to add 5 to a number. Compose these functions to create a new function that squares a number, doubles the result, and then adds 5.
+
+
 // square a number
 function square(x) {
   return x * x;
