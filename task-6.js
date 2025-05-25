f12f3d5069cdbc5d8938e265231d4957aa9114c9
Mizanur Rahman <mizanrxb@gmail.com>
2025-05-25 22:17:17 +0600

    description 6 added

--- task-6.js
+++ task-6.js
@@ -1,3 +1,8 @@
+// Leap Year Checker
+// Write a function that determines whether a given year is a leap year.
+// Example: Happy New Year
+
+
 function isLeapYear(year) {
   if ((year % 4 === 0 && year % 100 !== 0) || year % 400 === 0) {
     return true;
