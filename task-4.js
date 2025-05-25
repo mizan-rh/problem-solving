a23cfc64649b6c18f7609241eea8bfc3f9bdb4c2
Mizanur Rahman <mizanrxb@gmail.com>
2025-05-25 22:15:50 +0600

    description 4 added

--- task-4.js
+++ task-4.js
@@ -1,3 +1,8 @@
+// Sorting Objects
+// Create an array of objects representing cars with properties like make, model, and year. Write a function to sort the array of cars by the year of manufacture in ascending order. Print the sorted array.
+
+
+
 // Array of car objects
 const cars = [
   { make: "Toyota", model: "Corolla", year: 2020 },
